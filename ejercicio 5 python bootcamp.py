1b48d308a50722a1fe3997e1588e1616028f55c0
RoxQ25 <111744804+RoxQ25@users.noreply.github.com>
2022-08-31 17:18:59 +0200

    Add files via upload
    ejercicio 5 sobre python openbootcamp

---
+++ ejercicio 5 python bootcamp.py
@@ -0,0 +1,23 @@
+Python 3.10.6 (tags/v3.10.6:9c7b4bd, Aug  1 2022, 21:53:49) [MSC v.1932 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+nombre = "Pepe"
+apellidos = "Martínez Castro"
+edad = 29
+email = "pepemartcas93#gmail.com"
+casado = True
+hijos = False
+lista_amigos = ["Noelia", "Maite", "David", "Santi", "Dani", "Blanca"]
+películas_vistas = {
+    "película": "avatar",
+    "película2": "Gladiator",
+    "película3": "Intocable"
+    }
+
+print (nombre)
+print (apellidos)
+print (edad)
+print (email)
+print (lista_amigos)
+print (películas_vistas["película2"])
+print (casado)
+print (hijos)
