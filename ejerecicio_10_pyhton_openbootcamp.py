05a23195940e9fef1c7378f2b16ec3d5a66bd454
RoxQ25 <111744804+RoxQ25@users.noreply.github.com>
2022-09-02 13:29:08 +0200

    Add files via upload
    ejercicio_10_pyhton_openbootcamp

---
+++ ejerecicio_10_pyhton_openbootcamp.py
@@ -0,0 +1,14 @@
+
+# Online Python - IDE, Editor, Compiler, Interpreter
+base = input ("dime la base de este triángulo en cm: ")
+altura = input ("dime la altura de este triángulo en cm: ")
+
+área_triángulo = int (base) * int (altura) / 2
+print ("el área de este triángulo es de", (área_triángulo), "cm")
+
+radio = float(input("dime el radio de este círculo en cm: "))
+pi =  3.14
+
+área_círculo = pi*radio**2
+print ("el área de este círculo es de", (área_círculo), "cm")
+
