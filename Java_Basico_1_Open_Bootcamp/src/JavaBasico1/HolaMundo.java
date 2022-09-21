a516540586791ff0a021f45af93eaf6614e092e6
RoxQ <111744804+RoxQ25@users.noreply.github.com>
2022-09-21 13:41:53 +0200

    ejercicio 1 javabasico openbootcamp

---
+++ Java_Basico_1_Open_Bootcamp/src/JavaBasico1/HolaMundo.java
@@ -0,0 +1,36 @@
+package JavaBasico1;
+
+public class HolaMundo {
+
+	public static void main(String[] args){
+		//VARIABLESNUMERICAS
+			//ENTEROS
+			byte numero1 = 5;
+			System.out.println(numero1);
+			short numero2 = 10;
+			System.out.println(numero2);
+			int numero3 = 30;
+			System.out.println(numero3);
+			long  numero4 = 100;
+			System.out.println(numero4);
+
+			//DECIMALES
+			float numero5 = 5.5f;
+			System.out.println(numero5);
+			double numero6 = 10.5d;
+			System.out.println(numero6);
+
+			//BOOLEANO
+			boolean variable1 = false;
+			System.out.println(variable1);
+			boolean variable2 = true;
+			System.out.println(variable2);
+
+			//TEXTO
+			char variable3 = 'Z';
+			System.out.println(variable3);
+			String variable4 = "En un lugar de la Mancha....";
+			System.out.println(variable4);
+
+	}
+}
