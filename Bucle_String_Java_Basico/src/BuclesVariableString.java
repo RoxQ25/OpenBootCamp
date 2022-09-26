499e3be074b0aa7574b31fe141f2baecb49487a0
RoxQ <111744804+RoxQ25@users.noreply.github.com>
2022-09-26 11:26:33 +0200

    Java Basico Open Bootcamp 3 Bucles String

---
+++ Bucle_String_Java_Basico/src/BuclesVariableString.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class BuclesVariableString {
+	public static void main(String[] args) {
+		Scanner leer = new Scanner(System.in);
+		String n = "José";
+		while (n.equals("José")) {
+			System.out.println("Ingrese su nombre: ");
+			String nom = leer.next();
+			if (n.equals(nom)) {
+				System.out.println("Bienvenido: "+nom);
+				break;
+			} else{
+				System.out.println("no le hemos identificado, vuelva a probar");
+			}
+		}
+	}
+}
